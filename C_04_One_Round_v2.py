f8f8f91ec2cb5823131f0ad5311ef5b305856eed
luo <chenl24889@masseyhigh.school.nz>
2026-02-13 09:41:52 +0800

    working single rounds

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -19,6 +19,13 @@ def initial_points(which_player):
     return total, double
 
 
+def make_statement(statement, decoration):
+    """ADDa emoji / additional characters to the start and end of heading"""
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
+
+
+
 # Main starts here...
 
 # Roll the dice for the user and note if they got a double
@@ -78,7 +85,34 @@ while player_1_points < 13 and player_2_points < 13:
 
     print(f"{first}: {player_1_points} | {second} {player_2_points}")
 
-print("end of round")
+# end of round
+
+# associate player points with either the user or the computer
+user_points = player_1_points
+comp_points = player_2_points
+
+# switch the user and computer points if the computer went first
+if first == "computer":
+    user_points, comp_points = comp_points, user_points
+
+# work out who won...
+if user_points > comp_points:
+    winner = "user"
+else:
+    winner = "computer"
+
+round_feedback = f"The {winner} won."
+
+# double user points if eligible
+if winner == "user" and double_user == "yes":
+    user_points = user_points * 2
+
+# Output round results
+make_statement("Round Results","=")
+print("\nRound results")
+print(f"User Points: {user_points} | Computer Points: {comp_points}")
+print(round_feedback)
+print()
 
 
 
